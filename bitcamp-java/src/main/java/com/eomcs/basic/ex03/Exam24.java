6ed6da6b0a5a271b7fd6959b05a2beaeca7d1e8f
jeoneunhye <onsetttt@gmail.com>
2019-12-11 20:15:58 +0900

    정수 리터럴 저장방법

--- bitcamp-java/src/main/java/com/eomcs/basic/ex03/Exam24.java
+++ bitcamp-java/src/main/java/com/eomcs/basic/ex03/Exam24.java
@@ -51,25 +51,5 @@ public class Exam24 {
     //   ★2의 보수를 만드는 방법
     //   오른쪽에서부터 1을 찾는다. 찾은 1의 왼쪽편에 있는 모든 비트를 반대 값으로 바꾼다.
     //   예) 0010 1100(44) 1101 0100(-44)
-
-
-    System.out.println(-2147483648); // -2의 31승 : 4바이트 최소 음수
-    System.out.println(2147483647); // 2**31 - 1 : 4바이트 최대
-
-    //System.out.println(-2147483649);  // 4바이트 메모리 크기 초과
-    //System.out.println(2147483648); // 오류 발생 : integer number too large
-
-    // 8바이트 정수 리터럴 : 숫자 뒤에 접미사 L. 엘을 붙인다
-    System.out.println(-9_223_372_036_854_775_808L);  
-    System.out.println(-9_223_372_036_854_775_808l);
-    System.out.println(9_223_372_036_854_775_807L);
-
-    System.out.println(100);  // 4바이트 정수 리터럴
-    System.out.println(100L); // 8바이트 정수 리터럴
-
-    System.out.println(Integer.MIN_VALUE);  // 4바이트 최소 정수값
-    System.out.println(Integer.MAX_VALUE);  // 4바이트 최대 정수값
-    System.out.println(Long.MIN_VALUE);  // 8바이트 최소 정수값
-    System.out.println(Long.MAX_VALUE);  // 8바이트 최대 정수값
   }
 }
