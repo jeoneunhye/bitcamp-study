b5a84e4d42aac11b236d879ddcc81a46221f29a8
eunhdev@gmail.com <eunhdev@gmail.com>
2020-02-17 21:00:23 +0900

    synchronized

---
+++ bitcamp-java/src/main/java/com/eomcs/concurrent/ex5/ATM.java
@@ -0,0 +1,25 @@
+package com.eomcs.concurrent.ex5;
+
+public class ATM extends Thread {
+  Account account;
+
+  public ATM(String name, Account account) {
+    super(name);
+    this.account = account;
+  }
+
+  @Override
+  public void run() {
+    long money = 0;
+    long sum = 0;
+
+    while (true) {
+      money = account.withdraw(100); // 계좌에서 100원을 출금
+      if (money <= 0)
+        break;
+      sum += money;
+    }
+
+    System.out.printf("%s에서 찾은 돈: %d원\n", this.getName(), sum);
+  }
+}
