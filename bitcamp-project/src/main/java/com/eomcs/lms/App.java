8ff52f3c2e5a47c0378578f3aa3be2126c53055c
jeoneunhye <eunhdev@gmail.com>
2020-02-28 19:43:25 +0900

    v31_3 애플리케이션의 시작과 종료 시점에 안내 문구를 출력하는 옵저버 추가하기

---
+++ bitcamp-project/src/main/java/com/eomcs/lms/App.java
@@ -0,0 +1,165 @@
+package com.eomcs.lms;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Set;
+import com.eomcs.context.ApplicationContextListener;
+import com.eomcs.lms.domain.Board;
+import com.eomcs.lms.domain.Lesson;
+import com.eomcs.lms.domain.Member;
+import com.eomcs.lms.handler.BoardAddCommand;
+import com.eomcs.lms.handler.BoardDeleteCommand;
+import com.eomcs.lms.handler.BoardDetailCommand;
+import com.eomcs.lms.handler.BoardListCommand;
+import com.eomcs.lms.handler.BoardUpdateCommand;
+import com.eomcs.lms.handler.Command;
+import com.eomcs.lms.handler.ComputePlusCommand;
+import com.eomcs.lms.handler.HelloCommand;
+import com.eomcs.lms.handler.LessonAddCommand;
+import com.eomcs.lms.handler.LessonDeleteCommand;
+import com.eomcs.lms.handler.LessonDetailCommand;
+import com.eomcs.lms.handler.LessonListCommand;
+import com.eomcs.lms.handler.LessonUpdateCommand;
+import com.eomcs.lms.handler.MemberAddCommand;
+import com.eomcs.lms.handler.MemberDeleteCommand;
+import com.eomcs.lms.handler.MemberDetailCommand;
+import com.eomcs.lms.handler.MemberListCommand;
+import com.eomcs.lms.handler.MemberUpdateCommand;
+import com.eomcs.util.Prompt;
+
+public class App {
+  static Scanner keyboard = new Scanner(System.in);
+  static Deque<String> commandStack = new ArrayDeque<>();
+  static Queue<String> commandQueue = new LinkedList<>();
+
+  Set<ApplicationContextListener> listeners = new HashSet<>();
+
+  Map<String, Object> context = new HashMap<>();
+
+  public void addApplicationContextListener(ApplicationContextListener listener) {
+    listeners.add(listener);
+  }
+
+  public void removeApplicationContextListener(ApplicationContextListener listener) {
+    listeners.remove(listener);
+  }
+
+  private void notifyApplicationInitialized() {
+    for (ApplicationContextListener listener : listeners) {
+      listener.contextInitialized(context);
+    }
+  }
+
+  private void notifyApplicationDestroyed() {
+    for (ApplicationContextListener listener : listeners) {
+      listener.contextDestroyed(context);
+    }
+  }
+
+  @SuppressWarnings("unchecked")
+  public void service() {
+    notifyApplicationInitialized();
+
+    List<Lesson> lessonList = (List<Lesson>) context.get("lessonList");
+    List<Member> memberList = (List<Member>) context.get("memberList");
+    List<Board> boardList = (List<Board>) context.get("boardList");
+
+    Prompt prompt = new Prompt(keyboard);
+
+    Map<String, Command> commandMap = new HashMap<>();
+
+    commandMap.put("/lesson/add", new LessonAddCommand(prompt, lessonList));
+    commandMap.put("/lesson/list", new LessonListCommand(lessonList));
+    commandMap.put("/lesson/detail", new LessonDetailCommand(prompt, lessonList));
+    commandMap.put("/lesson/update", new LessonUpdateCommand(prompt, lessonList));
+    commandMap.put("/lesson/delete", new LessonDeleteCommand(prompt, lessonList));
+
+    commandMap.put("/member/add", new MemberAddCommand(prompt, memberList));
+    commandMap.put("/member/list", new MemberListCommand(memberList));
+    commandMap.put("/member/detail", new MemberDetailCommand(prompt, memberList));
+    commandMap.put("/member/update", new MemberUpdateCommand(prompt, memberList));
+    commandMap.put("/member/delete", new MemberDeleteCommand(prompt, memberList));
+
+    commandMap.put("/board/add", new BoardAddCommand(prompt, boardList));
+    commandMap.put("/board/list", new BoardListCommand(boardList));
+    commandMap.put("/board/detail", new BoardDetailCommand(prompt, boardList));
+    commandMap.put("/board/update", new BoardUpdateCommand(prompt, boardList));
+    commandMap.put("/board/delete", new BoardDeleteCommand(prompt, boardList));
+
+    commandMap.put("/hello", new HelloCommand(prompt));
+    commandMap.put("/compute/plus", new ComputePlusCommand(prompt));
+
+    String command;
+
+    while (true) {
+      System.out.print("\n명령> ");
+      command = keyboard.nextLine();
+
+      if (command.length() == 0)
+        continue;
+
+      if (command.equals("quit")) {
+        break;
+
+      } else if (command.equals("history")) {
+        printCommandHistory(commandStack.iterator());
+        continue;
+
+      } else if (command.equals("history2")) {
+        printCommandHistory(commandQueue.iterator());
+        continue;
+      }
+
+      commandStack.push(command);
+      commandQueue.offer(command);
+
+      Command commandHandler = commandMap.get(command);
+      if (commandHandler != null) {
+        try {
+          commandHandler.execute();
+
+        } catch (Exception e) {
+          System.out.printf("명령어 실행 중 오류 발생: %s\n", e.getMessage());
+        }
+
+      } else {
+        System.out.println("실행할 수 없는 명령입니다.");
+      }
+    }
+
+    keyboard.close();
+
+    notifyApplicationDestroyed();
+  }
+
+  private static void printCommandHistory(Iterator<String> iterator) {
+    int count = 0;
+
+    while (iterator.hasNext()) {
+      System.out.println(iterator.next());
+      count++;
+
+      if (count % 5 == 0) {
+        System.out.print(":");
+        String str = keyboard.nextLine();
+        if (str.equalsIgnoreCase("q"))
+          break;
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    App app = new App();
+    app.addApplicationContextListener(new DataLoaderListener());
+    app.addApplicationContextListener(new GreetingListener());
+    app.service();
+  }
+}
