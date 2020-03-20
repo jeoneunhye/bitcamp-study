e3e0ea1fd57c5611c2ec7be9255298520cade102
jeoneunhye <eunhdev@gmail.com>
2020-03-20 18:04:02 +0900

    Spring IoC 컨테이너로 프로퍼티 에디터 사용하기

---
+++ bitcamp-java/src/main/java/com/eomcs/spring/ioc/ex07/c/Exam01.java
@@ -0,0 +1,16 @@
+// 프로퍼티 에디터 - String ==> java.sql.Date ; 변환기 사용하여 처리하기
+package com.eomcs.spring.ioc.ex07.c;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Exam01 {
+  public static void main(String[] args) {
+    ApplicationContext iocContainer = new ClassPathXmlApplicationContext(
+        "com/eomcs/spring/ioc/ex07/c/application-context.xml");
+
+    System.out.println(iocContainer.getBean("c1"));
+  }
+
+  // CustomDateEditor.setAsText()를 오버라이딩하여 문자열을 원하는 type으로 만든다.
+}
