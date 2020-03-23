3b7b270a5d38e85b906b2bb24e148744b86e8d62
jeoneunhye <eunhdev@gmail.com>
2020-03-23 10:05:24 +0900

    Spring IoC 컨테이너로 컬렉션 타입의 프로퍼티 값 설정하기

---
+++ bitcamp-java/src/main/java/com/eomcs/spring/ioc/ex05/c/Exam01.java
@@ -0,0 +1,17 @@
+// 컬렉션 타입의 프로퍼티 값 설정 - Map<,>
+package com.eomcs.spring.ioc.ex05.c;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Exam01 {
+  public static void main(String[] args) {
+    ApplicationContext iocContainer = new ClassPathXmlApplicationContext(
+        "com/eomcs/spring/ioc/ex05/c/application-context.xml");
+
+    System.out.println(iocContainer.getBean("c1"));
+
+    // Car 객체 안에 options Map의 값을 넣어 설정하기
+    // => entry 태그 사용
+  }
+}
