b6256a15cf27932bdc9fc61ca3c3df59173291b6
jeoneunhye <eunhdev@gmail.com>
2020-03-23 18:14:15 +0900

    Spring 설정 정보가 담긴 AppConfig에 @ComponentScan으로 생성할 객체의 패키지나 클래스 지정하기

---
+++ bitcamp-java/src/main/java/com/eomcs/spring/ioc/ex11/p1/Controller1.java
@@ -0,0 +1,11 @@
+package com.eomcs.spring.ioc.ex11.p1;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+// MVC 아키텍처의 컨트롤러 객체를 표시할 때 붙인다.
+// 개발자가 유지보수할 때 클래스의 역할을
+// 더 정확히 이해할 수 있게 도와주기 위해 사용하는 애노테이션이다.
+public class Controller1 {
+
+}
